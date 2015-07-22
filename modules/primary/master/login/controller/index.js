2081bf335bdce7404f451d2f3b5e6ebea8e9572e
Salem MOSBAHI <salemmosbahi@gmail.com>
2015-07-22 21:02:53 +0100

    finish login system

--- modules/primary/master/login/controller/index.js
+++ modules/primary/master/login/controller/index.js
@@ -6,7 +6,8 @@ module.tr = JSON.parse(fs.readFileSync(__dirname + '/translation.json', 'utf8'))
 mod.server.router.use('/assets/login', mod.server.express.static(__dirname + '/../public'));
 
 mod.server.router.get("/login", function (req, res) {
-  res.render(__dirname + "/../views/view_1", {tr: module.tr["en"]});
+  if (req.session && req.session.user) res.redirect('/');
+  else res.render(__dirname + "/../views/view_1", {tr: module.tr["en"]});
 });
 
 mod.server.router.post("/login", function (req, res) {
@@ -15,13 +16,14 @@ mod.server.router.post("/login", function (req, res) {
       if (user) {
         if (req.body.password == user.password) {
           req.session.user = user;
-          res.send({"err": false});
-        }
-      }
+          res.send({"err": false, params: {goto: "/"}});
+        }else res.send({"err": {password: "not_exist"}});
+      }else res.send({"err": {username: "not_exist"}});
     }else res.send({"err":err});
   });
 });
 
-mod.server.router.post("/logout", function (req, res) {
+mod.server.router.get("/logout", function (req, res) {
   req.session.destroy();
+  res.redirect('/login');
 });
--- modules/primary/master/login/public/script_1.js
+++ modules/primary/master/login/public/script_1.js
@@ -1,9 +1,16 @@
 $(".login_form").submit(function (e) {
   e.preventDefault();
+  $(".progress").show();
   $.post(location.href, $(this).serialize(), function (r) {
-    if (r.err) console.log(r.err);
-    else {
-      location.href = "/";
+    if (r.err.username) {
+      $(".progress").hide();
+    } else if (r.err.password) {
+      $(".progress").hide();
+    } else if(!r.err) {
+      location.href = r.params.goto;
+    } else {
+      $(".progress").hide();
+      console.log(r.err);
     }
   });
 });
