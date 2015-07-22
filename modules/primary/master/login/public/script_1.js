bdc9f5f0dbfa0aa07b64e8001672fe490a96a1f6
Dardouri Mohamed <dmed@mahd.tn>
2015-07-23 00:29:45 +0100

    add button loader animation

--- modules/primary/master/login/public/script_1.js
+++ modules/primary/master/login/public/script_1.js
@@ -1,18 +1,20 @@
 $(".login_form").submit(function (e) {
   e.preventDefault();
-  $(".progress").show();
+  $(".login_btn>.preloader-wrapper").addClass("active").removeClass('hide');
   $.post(location.href, $(this).serialize(), function (r) {
     if (r.err.username) {
-      $(".progress").hide();
+      $(".login_btn>.preloader-wrapper").addClass("hide").removeClass('active');
       $("[name=username]").addClass('invalid');
     } else if (r.err.password) {
-      $(".progress").hide();
+      $(".login_btn>.preloader-wrapper").addClass("hide").removeClass('active');
       $("[name=password]").addClass('invalid');
     } else if(!r.err) {
       location.href = r.params.goto;
     } else {
-      $(".progress").hide();
+      $(".login_btn>.preloader-wrapper").addClass("hide").removeClass('active');
       console.log(r.err);
     }
   });
 });
+
+$("[name=username]").focus();
