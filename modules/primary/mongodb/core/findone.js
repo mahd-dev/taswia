e0431f058f49dfc285c1ce1f95ef4929e55c33ad
Dardouri Mohamed <dmed@mahd.tn>
2015-07-22 15:52:24 +0100

    add findOne function

---
+++ modules/primary/mongodb/core/findone.js
@@ -0,0 +1,5 @@
+module.exports = function (collection, query, projection, version) {
+  if(version===undefined && query["_id.version"] === undefined) query["_id.version"] = 0;
+  else query["_id.version"] = version;
+  return module.parent.mongodb.collection(collection).findOne(query, projection);
+};
